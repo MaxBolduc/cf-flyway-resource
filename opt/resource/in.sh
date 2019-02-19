f0f1f7008b57776f2fbba647b311360a59e2b0db
Jean-Philippe Morin <animationjpm@gmail.com>
2019-02-18 22:55:36 -0500

    Updated metadata with more service information

--- opt/resource/in.sh
+++ opt/resource/in.sh
@@ -19,16 +19,16 @@ cat < $request | jq -r . | sed "s/\"password\":.*/\"password\": \"**************
 PCF_API=$(jq -r '.source.api // empty' < $request)
 PCF_ORG=$(jq -r '.source.organization // empty' < $request)
 PCF_SPACE=$(jq -r '.source.space // empty' < $request)
+PCF_SERVICE=$(jq -r '.source.service // empty' < $request)
 PCF_USERNAME=$(jq -r '.source.username // empty' < $request)
 PCF_PASSWORD=$(jq -r '.source.password // empty' < $request)
-PCF_SERVICE=$(jq -r '.source.service // empty' < $request)
 
 [[ ! -z "$PCF_API" ]]             && echo "PCF_API : $PCF_API" || echo "'source.api' must be set to the PCF API endpoint!"
 [[ ! -z "$PCF_ORG" ]]             && echo "PCF_ORG : $PCF_ORG" || echo "'source.organization' must be set to the organization for PCF deployment!"
 [[ ! -z "$PCF_SPACE" ]]           && echo "PCF_SPACE : $PCF_SPACE" || echo "'source.space' must be set to the space for PCF deployment!"
-[[ ! -z "$PCF_USERNAME" ]]        && echo "PCF_USERNAME : $PCF_USERNAME" || echo "'source.user' must be set to the username for PCF deployment!"
-[[ ! -z "$PCF_PASSWORD" ]]        && echo "PCF_PASSWORD : ********************" || echo "'source.password' must be set to the password for PCF deployment!"
 [[ ! -z "$PCF_SERVICE" ]]         && echo "PCF_SERVICE : $PCF_SERVICE" || echo "'source.service' the database service instance name."
+[[ ! -z "$PCF_USERNAME" ]]        && echo "PCF_USERNAME : $PCF_USERNAME" || echo "'source.username' must be set to the username for PCF deployment!"
+[[ ! -z "$PCF_PASSWORD" ]]        && echo "PCF_PASSWORD : ********************" || echo "'source.password' must be set to the password for PCF deployment!"
 
 # --------------------------------
 
@@ -36,21 +36,25 @@ PCF_SERVICE=$(jq -r '.source.service // empty' < $request)
 cf login -a $PCF_API -u $PCF_USERNAME -p $PCF_PASSWORD -o $PCF_ORG -s $PCF_SPACE
 
 version=$(jq -r '.version.ref' < $request)
-
 service_guid=$(cf service $PCF_SERVICE --guid)
+metadata=$(cf curl /v2/service_instances/$service_guid | jq .)
 
-metadata=$(cf curl /v2/service_instances/$service_guid | jq .metadata)
-
-output=$(echo $metadata | jq -r "
+output=$(jq -n "
 {
     version: {
         ref: \"$version\"
     },
     metadata: [
-        {name: \"guid\", value: .guid},
-        {name: \"url\", value: .url},
-        {name: \"created_at\", value: .created_at},
-        {name: \"updated_at\", value: .updated_at}
+        {name: \"metadata_guid\", value: $(echo $metadata | jq .metadata.guid)},
+        {name: \"metadata_url\", value: $(echo $metadata | jq .metadata.url)},
+        {name: \"metadata_created_at\", value: $(echo $metadata | jq .metadata.created_at)},
+        {name: \"metadata_updated_at\", value: $(echo $metadata | jq .metadata.updated_at)},
+        {name: \"pcf_api\", value: \"$PCF_API\"},
+        {name: \"pcf_org\", value: \"$PCF_ORG\"},
+        {name: \"pcf_space\", value: \"$PCF_SPACE\"},
+        {name: \"service\", value: $(cf curl $(echo $metadata | jq -r .entity.service_url) | jq .entity.label)},
+        {name: \"service_instance\", value: $(echo $metadata | jq .entity.name)},
+        {name: \"service_plan\", value: $(cf curl $(echo $metadata | jq -r .entity.service_plan_url) | jq .entity.name)}
     ]
 }")
 
